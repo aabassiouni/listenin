799f87633b81aba25d4dac9ff250fb5c6649083d
Ali Bassiouni <aabassiouni@hotmail.com>
2024-01-30 03:33:36 +0200

    maybe dont listen
    rename server to index
    
    move to server folder

--- server/api/routes/refresh-token.js
+++ server/api/routes/refresh-token.js
@@ -18,7 +18,7 @@ router.get("/", function (req, res) {
 	}
 	let authOptions = {
 		url: "https://accounts.spotify.com/api/token",
-		headers: { Authorization: "Basic " + new Buffer(client_id + ":" + client_secret).toString("base64") },
+		headers: { Authorization: "Basic " + new Buffer.from(client_id + ":" + client_secret).toString("base64") },
 		form: {
 			grant_type: "refresh_token",
 			refresh_token: refresh_token,
